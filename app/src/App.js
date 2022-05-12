462cb299900ba65aa56ba1ab92433646691f4838
Mahesh Kasbe <maheshkasbe010@gmail.com>
2022-05-12 20:14:07 +0530

    first commit

---
+++ app/src/App.js
@@ -0,0 +1,40 @@
+import React,{ useState } from 'react';
+import './App.css';
+
+
+function App() {
+  const [count , setcount] = useState(0);
+  const inc = () => {
+    setcount(count+1)
+  }
+  const dec = () => {
+    setcount(count-1)
+  }
+
+  const inc2 = () => {
+    setcount(count+2)
+  }
+  const dec2 = () => {
+    setcount(count-2)
+  }
+
+  return (
+    <div className="App">
+        <h1>Counter App</h1>
+
+      <div className='counter'>
+
+      <button className='inc' onClick={inc2}> +2 </button>
+      <button className='inc' onClick={inc}> +1 </button>
+        <h1>{count}</h1>
+      <button className='dec' onClick={dec}> -1 </button>
+      <button className='dec' onClick={dec2}> -2 </button>
+      
+      </div>
+
+        
+    </div>
+  );
+}
+
+export default App;
